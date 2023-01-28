8f333ac5c3e26ea5cbcc02afdb467af93239f420
Justin Taylor <ju.tay.lor2019@gmail.com>
2023-01-28 02:34:52 -0800

    cleaned up CRA

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import io from "socket.io-client"
+import { useEffect, useRef, useState } from 'react';
+
+const socket = io.connect("http://localhost:3000");
 
 function App() {
+  const [userInput, setUserInput] = useState("")
+  const [newMsg, setNewMsg] = useState("");
+  const [time, setTime] = useState("");
+
+  const input = useRef('')
+  const sendMsg = (e) => {
+    e.preventDefault();
+    if (userInput){
+      socket.emit("message", {message:userInput, time:new Date().getDate()})
+      input.current.value = ""
+    }
+  }
+  const messages = useRef("");
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      var receivedMsg = document.createElement("li");
+      receivedMsg.textContent = data.time && data.message;
+      messages.current.appendChild(receivedMsg);
+    })
+  },[socket])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <form>
+        <input ref={input} placeholder="Message..." onChange={(e)=>{setUserInput(e.target.value)}}></input>
+        <button onClick={sendMsg}>Send</button>
+        <p>Messages:</p>
+        <ul ref={messages}></ul>
+      </form>
     </div>
   );
 }
